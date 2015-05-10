3d4c39f72223fb9a8e9a4002d3148a2b37716702
Thiago Senna <trsenna@gmail.com>
2015-05-10 13:15:08 -0300

    all other files reformatted

--- config/autoload/global.php
+++ config/autoload/global.php
@@ -2,15 +2,11 @@
 /**
  * Global Configuration Override
  *
- * You can use this file for overriding configuration values from modules, etc.
- * You would place values in here that are agnostic to the environment and not
- * sensitive to security.
+ * You can use this file for overriding configuration values from modules, etc. You would place values in here that are
+ * agnostic to the environment and not sensitive to security.
  *
- * @NOTE: In practice, this file will typically be INCLUDED in your source
- * control, so do not include passwords or other sensitive information in this
- * file.
+ * @NOTE: In practice, this file will typically be INCLUDED in your sourcevcontrol, so do not include passwords or
+ *        other sensitive information in this file.
  */
 
-return array(
-    // ...
-);
+return array();
